97a663b2266d8af0a0b7633464c037c85953ffc2
ㅤYV55 <yassine.ouzzane@etu.utc.fr>
2025-03-19 13:05:35 +0100

    Client Class created

---
+++ Classes/Client.java
@@ -0,0 +1,30 @@
+package Classes;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    private final Socket socket;
+    private final String pseudo;
+
+    public Client(Socket socket, String pseudo) {
+        this.socket = socket;
+        this.pseudo = pseudo;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket
+    }
+}
